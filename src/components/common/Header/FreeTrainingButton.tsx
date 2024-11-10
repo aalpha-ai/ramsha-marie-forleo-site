2b08103b5a7f530741f28324ca80b02534e95379
haasim vahora <haasimvahora@gmail.com>
2024-11-10 13:33:31 +0700

    feat: add responsiveness to lead magnet

--- src/components/common/Header/FreeTrainingButton.tsx
+++ src/components/common/Header/FreeTrainingButton.tsx
@@ -5,7 +5,7 @@ import React from "react";
 
 const FreeTrainingButton: React.FC = () => {
   return (
-    <button className="px-6 pt-2.5 pb-3 text-base font-bold  text-center text-black capitalize bg-yellow-200 rounded-3xl shadow-button cursor-pointer hover:shadow-button-hover max-md:px-5 font-area-normal">
+    <button className="whitespace-nowrap px-6 pt-2.5 pb-3 text-base font-bold  text-center text-black capitalize bg-yellow-200 rounded-3xl shadow-button cursor-pointer hover:shadow-button-hover max-md:px-5 font-area-normal">
       <a
         href="https://www.marieforleo.com/how-to-get-anything-you-want"
         target="_blank"
