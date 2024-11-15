a338a9d1f5dd818dfc0bc26833c47c1036ac67ba
haasim vahora <haasimvahora@gmail.com>
2024-11-15 12:57:52 +0700

    fix: revise animation on Learn Anything

--- src/components/pages/Main/LearnAnything/index.tsx
+++ src/components/pages/Main/LearnAnything/index.tsx
@@ -9,16 +9,18 @@ import BlurFade from "@/components/magicui/blur-fade";
 const LearnAnything: React.FC = () => {
   return (
     <main className="bg-stone-50 md:px-10 lg:px-0">
-      <BlurFade>
         <section className="flex flex-col md:flex-row py-20 lg:max-w-[1000px] mx-auto max-md:px-5">
-          <div className="w-full md:w-1/2">
-            <LearnHeader />
-          </div>
-          <div className="w-full md:w-1/2 flex justify-end items-center">
-            <LearnImage />
-          </div>
+          <BlurFade inViewMargin="-200px">
+            <div className="w-full md:w-1/2">
+              <LearnHeader />
+            </div>
+          </BlurFade>
+          <BlurFade inViewMargin="-200px">
+            <div className="w-full md:w-1/2 flex justify-end items-center">
+              <LearnImage />
+            </div>
+          </BlurFade>
         </section>
-      </BlurFade>
     </main>
   );
 };
