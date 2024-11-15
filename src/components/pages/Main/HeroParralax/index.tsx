29716adf122926ecc1bd27081674171e65334473
haasim vahora <haasimvahora@gmail.com>
2024-11-15 17:53:17 +0700

    feat: fix parralax and hero section

--- src/components/pages/Main/HeroParralax/index.tsx
+++ src/components/pages/Main/HeroParralax/index.tsx
@@ -5,7 +5,17 @@ import React from "react";
 
 const HeroParralax: React.FC = () => {
   return (
-    <div className="bg-fixed bg-[url('/gridImages/image30.png')] bg-cover bg-[50%_100%] bg-no-repeat w-full min-h-[600px]" />
+    <div 
+      className="
+        bg-[url('/gridImages/image30.png')] 
+        bg-cover 
+        bg-[50%_100%] 
+        bg-no-repeat 
+        w-full 
+        min-h-[600px]
+        md:bg-fixed
+      " 
+    />
   );
 };
 
