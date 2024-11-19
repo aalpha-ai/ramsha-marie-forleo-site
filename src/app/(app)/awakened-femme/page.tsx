1a07dedc9d2457077fe259ca74ad4b38b7fcb3a8
hvahora <haasimvahora@gmail.com>
2024-11-19 15:15:46 +0700

    feat: add animation to awakened femme lead magnet

--- src/app/(app)/awakened-femme/page.tsx
+++ src/app/(app)/awakened-femme/page.tsx
@@ -1,10 +1,17 @@
+'use client';
+
 import React from "react";
 import AwakenedFemmeLeadMagnet from "@/components/pages/AwakenedFemmeLeadMagnet";
 
 export default function Page() {
   return (
-    <>
+    <div className="min-h-screen bg-black overflow-auto" style={{ backgroundColor: 'black' }}>
       <AwakenedFemmeLeadMagnet />
-    </>
+      <style jsx global>{`
+        body {
+          background-color: black;
+        }
+      `}</style>
+    </div>
   );
 }
--- src/components/pages/FreeHypnosisLeadMagnet/index.tsx
+++ src/components/pages/FreeHypnosisLeadMagnet/index.tsx
@@ -7,7 +7,7 @@ import ImageSection from "./ImageSection";
 export default function Component() {
   return (
     <section>
-      <section className="flex flex-col items-center bg-ramsha-accent border-red-200 border-ramsha-primary border-[10px] xl:px-[10px] xl:pt-[56px] xl:pb-[54px] px-[0px] pt-[45px] pb-[55px] rounded-[30px]">
+      <section className="flex flex-col items-center bg-ramsha-accent border-ramsha-accent m-4 border-[10px] xl:px-[10px] xl:pt-[56px] xl:pb-[54px] px-[0px] pt-[45px] pb-[55px] rounded-[30px]">
         <div className="flex lg:flex-row flex-col items-start pb-8 max-w-full">
           <section className="flex flex-col flex-1 shrink pt-4 text-black px-10">
             <Header />
