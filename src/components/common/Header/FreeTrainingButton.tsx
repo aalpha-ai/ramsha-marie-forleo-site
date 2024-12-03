ad3036062ef1582ad22608b613e05f6193ed86df
hvahora <haasimvahora@gmail.com>
2024-12-03 07:17:40 +0700

    feat: add about page

--- src/components/common/Header/FreeTrainingButton.tsx
+++ src/components/common/Header/FreeTrainingButton.tsx
@@ -11,7 +11,7 @@ const FreeTrainingButton: React.FC<FreeTrainingButtonProps> = ({ className = '' 
   return (
     <button className={`font-subheader w-full md:w-[250px] whitespace-nowrap lg:px-4 xl:px-8 pt-2.5 pb-3 lg:text-[14px] xl:text-base text-center text-white capitalize bg-ramsha-tertiary rounded-3xl shadow-button cursor-pointer hover:shadow-button-hover max-md:px-5 ${className}`}>
       <a
-        href="https://www.marieforleo.com/how-to-get-anything-you-want"
+        href="/free-hypnosis-lead"
         target="_blank"
         rel="noopener noreferrer"
       >
